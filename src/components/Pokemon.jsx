712fe473e75e0787b87960ad059ba72dc695341a
Benjamin <bjmcbride2@gmail.com>
2023-05-08 09:48:59 -0700

    updated fetch request

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -6,22 +6,23 @@ const Pokémon = () => {
   const getAllPokémon = async (e) => {
     e.preventDefault()
     let allPokémon = [];
-    let offset = 0;
-    const limit = 20;
-    // I didn't see until after that you can set the limit of pokemon that you can request at once,
-    // so I stuck with the default limit of 20, and requested all of the pokemon 20 at a time until
-    // data.next was null (at the last page).
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
     let data = await response.json();
     allPokémon = [...allPokémon, ...data.results];
-    console.log(data);
-    console.log(data.next);
-    while (data.next) {
-      offset += limit;
-      response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
-      data = await response.json();
-      allPokémon = [...allPokémon, ...data.results];
-    }
+    // let offset = 0;
+    // const limit = 20;
+    // // I didn't see until after that you can set the limit of pokemon that you can request at once,
+    // // so I stuck with the default limit of 20, and requested all of the pokemon 20 at a time until
+    // // data.next was null (at the last page).
+    // let response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
+    // let data = await response.json();
+    // allPokémon = [...allPokémon, ...data.results];
+    // while (data.next) {
+    //   offset += limit;
+    //   response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
+    //   data = await response.json();
+    //   allPokémon = [...allPokémon, ...data.results];
+    // }
     setPokémon(allPokémon)
     return
   }
